edc53a7f145e4198ff28a743e6dfed327f14cade
WVallejos <guille.vallejosr@gmail.com>
2023-11-09 00:17:26 +0800

    deploy

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer  = require('multer')
+const upload = multer({ dest: './public/data/uploads/' })
 require('dotenv').config()
 
 var app = express();
@@ -10,6 +12,17 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post('/api/fileanalyse', upload.single("upfile"), function (req, res) {
+  const { originalname, size, mimetype } = req.file;
+
+  // Respond with file information
+  res.json({
+    name: originalname,
+    size: size,
+    type: mimetype,
+  });
+})
 
 
 
